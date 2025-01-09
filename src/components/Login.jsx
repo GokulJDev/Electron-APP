27b925002523ac0470f67d8b84bb20fc473cbfa8
GOKUL J DEV <gokuljdev@gmail.com>
2025-01-10 01:13:37 +0530

    BeforeLogin_2

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -1,27 +1,36 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 import './Login.css';
 
 const LoginSidebar = ({ onClose }) => {
   return (
-    <div className="login-sidebar">
-      <button className="close-sidebar" onClick={onClose}>
-        &times;
-      </button>
-      <h2>Login</h2>
-      <form className="login-form">
-        <label>
-          Email/User name
-          <input type="text" placeholder="Enter your email or username" />
-        </label>
-        <label>
-          Password
-          <input type="password" placeholder="Enter your password" />
-        </label>
-        <button type="submit" className="login-submit">Login</button>
-      </form>
-      <a href="#" className="forgot-password">Forgot Password?</a>
+    <div className="modal-overlay">
+      <div className="modal-content">
+        <h2 className='wel'>Welcome Back!</h2>
+        <p className='para'>Enter your details to login and continue</p>
+        <form className="login-form">
+          <label>
+            Username
+            <input type="text" placeholder="example@gmail.com" />
+          </label>
+          <label>
+            Password
+            <input type="password" placeholder="********" />
+          </label>
+          <a href="/" className="forgot-password">Forgot password?</a>
+          <button type="submit" className="login-submit">Login</button>
+        </form>
+        <p className='para'>
+          Don’t have an account? <a href="www.google.com" className="signup-link">Get an Account</a>
+        </p>
+        <a href="/" className="skip-for-now" onClick={onClose}>Skip for now &rarr;</a>
+      </div>
     </div>
   );
+};
+
+LoginSidebar.propTypes = {
+  onClose: PropTypes.func.isRequired,
 };
 
 export default LoginSidebar;
